bc82f73184ffbbfd7e16e46f60ea71ed9b53fa95
summerrwalker <57104140+summerrwalker@users.noreply.github.com>
2019-10-28 15:17:42 +0000

    Created Hello and user input

---
+++ Hello.cpp
@@ -0,0 +1,9 @@
+#Summer Walker
+
+#include<iostream.h>
+void main()
+{ int age = 0;
+  cout<<"How old are you?";
+  cin >> age;
+  cout<<"Hello All, I am Summer Walker, I am " + age + " years old!";
+}
